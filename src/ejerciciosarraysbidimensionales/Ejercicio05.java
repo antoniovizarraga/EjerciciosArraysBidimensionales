f29b728905b885b91cb768be657ae5136e3920cf
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-02-05 10:30:37 +0100

    Ejercicio 5 terminado

--- src/ejerciciosarraysbidimensionales/Ejercicio05.java
+++ src/ejerciciosarraysbidimensionales/Ejercicio05.java
@@ -3,37 +3,50 @@ package ejerciciosarraysbidimensionales;
 import java.util.Random;
 import java.util.Arrays;
 
+/* Vamos a crear un programa que genere 20 valores aleatorios del 100 al 999,
+ * y los meta en un Array de 4 x 5. Para que luego, usando bucles for
+ * imprimamos la suma de cada columa y fila en su lugar
+ * correspondiente, además de indicar la suma total de
+ * todas las filas o de todas las columnas en la esquina
+ * inferior derecha. */
 public class Ejercicio05 {
-	
-	
+
+	// Función que genera un array bidimensional de 4x5 con números aleatorios entre
+	// 100 y 1000
 	public static int[][] randomArray() {
+		// Declaración del array bidimensional
 		int[][] table = new int[4][5];
-		
+
+		// Creación de un objeto Random para generar números aleatorios
 		Random rand = new Random();
-		
-		for(int i = 0; i < table.length; i++) {
-			for(int j = 0; j < table[0].length; j++) {
+
+		// Bucle para rellenar el array con números aleatorios
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table[0].length; j++) {
 				table[i][j] = rand.nextInt(100, 1000);
 			}
 		}
-		
+
+		// Devuelve el array generado
 		return table;
 	}
 
+	// Función principal
 	public static void main(String[] args) {
-		
-		
+
+		// Declaración del array bidimensional
 		int[][] table = new int[4][5];
-		
+
+		// Copia el array generado por la función randomArray en el array table
 		table = Arrays.copyOf(randomArray(), 4);
-		
+
+		// Variables para almacenar las sumas de las filas, las columnas y el total
 		int sumaFilas = 0;
 		int sumaColumnas = 0;
 		int sumaTotal = 0;
-		
-		
-		//table[3][4] = result;
-		
+
+		// Bucle para sumar los valores de las filas y el total, e imprimir los valores
+		// y la suma de cada fila
 		for (int[] fila : table) {
 			for (int valor : fila) {
 				sumaFilas += valor;
@@ -44,18 +57,18 @@ public class Ejercicio05 {
 			sumaFilas = 0;
 			System.out.println();
 		}
-		
-		for(int j = 0; j < table[0].length; j++ ) {
-			for(int i = 0; i < table.length; i++) {
+
+		// Bucle para sumar los valores de las columnas e imprimir la suma de cada
+		// columna
+		for (int j = 0; j < table[0].length; j++) {
+			for (int i = 0; i < table.length; i++) {
 				sumaColumnas += table[i][j];
 			}
 			System.out.print(sumaColumnas + "\t");
 			sumaColumnas = 0;
 		}
-		
-		System.out.print("Suma total: " + sumaTotal);
-		
-		
-	}
 
+		// Imprime la suma total de todos los valores del array
+		System.out.print("Suma total: " + sumaTotal);
+	}
 }
