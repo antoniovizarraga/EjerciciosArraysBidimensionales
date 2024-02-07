8844f931fbea7e2ea23c333a8de64796663c0c55
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2024-02-07 09:41:41 +0100

    Ejercicio 7 empezado

---
+++ src/ejerciciosarraysbidimensionales/Ejercicio07.java
@@ -0,0 +1,18 @@
+package ejerciciosarraysbidimensionales;
+
+public class Ejercicio07 {
+
+	public static int[][] transpositionOfArray (int[][] table) {
+		int[][] table2 = new int[table.length][table[0].length];
+		
+		
+		
+		return table2;
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
